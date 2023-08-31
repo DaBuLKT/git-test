f6737f8b0e03a42ba4526a7c88f76ff07ab87b03
LKT <2625886212@qq.com>
2023-08-31 11:52:31 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @author 连锴潼
+ * @version 1.0
+ * @className GitTest
+ * @date: 2023/8/31 11:48
+ * @since 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
